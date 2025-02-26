267c66ae4d913640b93479ffb4d64e225a9ea33a
doplerer <carlosmarinperez1@gmail.com>
2025-02-26 10:58:30 +0100

    SETUP

--- src/main/java/com/doplerer/ludor/Ludor.java
+++ src/main/java/com/doplerer/ludor/Ludor.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LudorApplication {
+public class Ludor {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LudorApplication.class, args);
+		SpringApplication.run(Ludor.class, args);
 	}
 
 }
---
+++ src/main/java/com/doplerer/ludor/controller/Game.java
@@ -0,0 +1,16 @@
+package com.doplerer.ludor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Game {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
