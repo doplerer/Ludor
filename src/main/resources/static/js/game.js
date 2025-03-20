b5b47f4123165495adf92698b9b92314b42f1aee
doplerer <carlosmarinperez1@gmail.com>
2025-03-21 00:01:57 +0100

    ws testing

--- src/main/resources/static/js/game.js
+++
@@ -1,4 +0,0 @@
-// WebSocket 
-const socket = new WebSocket("ws://localhost:8080/game", ["permessage-deflate"]);
-
-
---
+++ src/main/resources/static/js/ws.js
@@ -0,0 +1,25 @@
+// WebSocket 
+const socket = new WebSocket("ws://localhost:8080/game", ["permessage-deflate"]);
+
+// Evento cuando la conexión se establece
+socket.onopen = function () {
+    console.log("Conectado al WebSocket");
+    socket.send(JSON.stringify({username: "Jugador1"}));
+};
+
+// Evento cuando se recibe un mensaje
+socket.onmessage = function (event) {
+    console.log("Mensaje recibido:", event.data);
+};
+
+// Evento cuando la conexión se cierra
+socket.onclose = function () {
+    console.log("Desconectado del WebSocket");
+};
+
+// Evento en caso de error
+socket.onerror = function (error) {
+    console.error("Error en WebSocket", error);
+};
+
+
