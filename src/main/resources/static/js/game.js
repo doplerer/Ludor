c7c4a09fc95b5242b5b28df6241f850fd221bd7e
doplerer <carlosmarinperez1@gmail.com>
2025-05-13 19:52:17 +0200

    Jugadores y username dinámico

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -10,8 +10,45 @@ function displayGame(data) {
     if (document.getElementById("game").classList.contains("off")) {
         document.getElementById("game").classList.remove("off");
     }
+
+    // User's Data
+
+    var username = data.username;
+    var playerId = data.playerId;
     
-    console.log("Game status: " + data);
+    // Add Players
+    var players = document.getElementById("players");
+    players.innerHTML = "";
+    for (let i=0; i<data.players.length; i++){
+        let player = data.players[i];
+        console.log(player);
+
+        if (player.id ==  playerId){
+            break;
+        }
+
+        players.innerHTML += `<div class="player">
+                            <div class="profile">
+                                <img class="avatar" src="/svg/avatar.svg"></img>
+                                <div class="username">${player.username}</div>
+                            </div>
+                            <div class="deck">
+                                <img class="card" src="/svg/cards/BACK.svg">
+                                <img class="card" src="/svg/cards/BACK.svg">
+                                <img class="card" src="/svg/cards/BACK.svg">
+                                <img class="card" src="/svg/cards/BACK.svg">
+                                <img class="card" src="/svg/cards/BACK.svg">
+                            </div>
+                        </div>`
+
+    }
+
+    // User's Username
+    var usernameDiv = document.getElementById("username");
+    usernameDiv.textContent = username;
+
+    // Display cards
+    console.log(data.cards);
 
 
 }
